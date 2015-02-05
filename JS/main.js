d3484425cffff65e4b643f595f5c097833bbef1a
Joshua Shi <thehandsomepanther@gmail.com>
2015-02-04 19:36:50 -0600

    Updated navsquare

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
+    var nav_down = false;
+
     $(document).scroll(function () {
         $('#scroll').css('opacity', (100 - $(this).scrollTop()) / 100);
     });
+
+    $('.nav-square').click(function () {
+        if (nav_down) {
+            $('.nav-square').animate({
+                top: "-90px"
+            }, 200, function () {});
+            nav_down = false;
+        } else {
+            $('.nav-square').animate({
+                top: "0"
+            }, 200, function () {});
+            nav_down = true;
+        };
+    });
+
 });
