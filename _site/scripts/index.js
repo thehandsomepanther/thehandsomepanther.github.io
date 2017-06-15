858a2423b99d824685f71a1ae61bf8f66a9b87a3
Josh Shi <thehandsomepanther@gmail.com>
2017-06-15 19:11:52 -0400

    sort of works on mobile

--- _site/scripts/index.js
+++ _site/scripts/index.js
@@ -8,23 +8,23 @@
             }
             running = true;
             requestAnimationFrame(function() {
-                obj.dispatchEvent(new CustomEvent(name));
+                obj.dispatchEvent(new CustomEvent(name))
                 running = false;
-            });
+            })
         };
-        obj.addEventListener(type, func);
+        obj.addEventListener(type, func)
     };
 
     /* init - you can init any event */
-    throttle("resize", "optimizedResize");
-})();
+    throttle("resize", "optimizedResize")
+})()
 
 function removeEventListeners(e) {
-    var clone = e.cloneNode();
+    var clone = e.cloneNode()
     while (e.firstChild) {
-        clone.appendChild(e.lastChild);
+        clone.appendChild(e.lastChild)
     }
-    e.parentNode.replaceChild(clone, e);
+    e.parentNode.replaceChild(clone, e)
 }
 
 var THRESHOLD = 5;
@@ -87,18 +87,11 @@ var drawModes = {
 }
 var mode = drawModes.lines
 
-document.addEventListener('DOMContentLoaded', function() {
-    drawScreen()
-    document.querySelector('input[name="reset"]').addEventListener('mousedown', drawScreen)
-    document.querySelector('select').addEventListener('change', changeMode)
-    window.addEventListener("optimizedResize", drawScreen);
-})
-
 function evaluate(a, b) {
     if (Math.abs(a - b) < THRESHOLD) {
         return 'EQUAL'
     } else {
-        return a > b ? 'GREATER' : 'LESS';
+        return a > b ? 'GREATER' : 'LESS'
     }
 }
 
@@ -114,28 +107,28 @@ function draw(e) {
     }
 
     if (delta.x == 'GREATER' && delta.y == 'EQUAL') {
-        this.innerHTML = mode.right;
+        this.innerHTML = mode.right
     } else if (delta.x == 'LESS' && delta.y == 'EQUAL') {
-        this.innerHTML = mode.left;
+        this.innerHTML = mode.left
     } else if (delta.x == 'EQUAL' && delta.y == 'LESS') {
-        this.innerHTML = mode.up;
+        this.innerHTML = mode.up
     } else if (delta.x == 'EQUAL' && delta.y == 'GREATER') {
-        this.innerHTML = mode.down;
+        this.innerHTML = mode.down
     } else if (delta.x == 'GREATER' && delta.y == 'GREATER') {
-        this.innerHTML = mode.downright;
+        this.innerHTML = mode.downright
     } else if (delta.x == 'GREATER' && delta.y == 'LESS') {
-        this.innerHTML = mode.upright;
+        this.innerHTML = mode.upright
     } else if (delta.x == 'LESS' && delta.y == 'GREATER') {
-        this.innerHTML = mode.downleft;
+        this.innerHTML = mode.downleft
     } else if (delta.x == 'LESS' && delta.y == 'LESS') {
-        this.innerHTML = mode.upleft;
+        this.innerHTML = mode.upleft
     }
 
-    prev = curr;
+    prev = curr
 }
 
 function changeMode(e) {
-    mode = drawModes[e.currentTarget.value];
+    mode = drawModes[e.currentTarget.value]
 }
 
 function showImage(e) {
@@ -143,7 +136,7 @@ function showImage(e) {
     var imgElem = document.querySelector(imgClass)
 
     if (imgElem !== null) {
-        imgElem.style.display = 'block';
+        imgElem.style.display = 'block'
         if (imgElem.tagName === 'VIDEO') {
             imgElem.play()
         }
@@ -152,7 +145,7 @@ function showImage(e) {
 
 function hideImage(e) {
     for (var imgElem of document.querySelectorAll('[class^="img-"]')) {
-        imgElem.style.display = 'none';
+        imgElem.style.display = 'none'
 
         if (imgElem.tagName === 'VIDEO') {
             imgElem.pause()
@@ -185,12 +178,14 @@ function drawScreen() {
             parent.className = link.className
         }
 
-        for (var word of words) {
+        for (var i = 0; i < words.length; i++) {
+            var word = words[i]
+
             if (length + word.length > width) {
-                for (var i = 0; i < width - length; i++) {
+                for (var j = 0; j < width - length; j++) {
                     var span = document.createElement('span')
                     span.innerHTML = filler
-                    frag.appendChild(span)
+                    parent.appendChild(span)
                 }
 
                 length = 0
@@ -202,10 +197,14 @@ function drawScreen() {
                 parent.appendChild(span)
             }
 
-            var span = document.createElement('span')
-            span.innerHTML = filler
-            parent.appendChild(span)
-            length += word.length + 1
+            if (i !== words.length - 1) {
+                var span = document.createElement('span')
+                span.innerHTML = filler
+                parent.appendChild(span)
+                length += word.length + 1
+            } else {
+                length += word.length
+            }
         }
 
         if (parent !== frag) {
@@ -233,3 +232,63 @@ function drawScreen() {
         link.addEventListener('mouseleave', hideImage)
     }
 }
+
+function triggerEvent(el, type, obj){
+   if ('createEvent' in document) {
+        // modern browsers, IE9+
+        var e = new Event(type);
+        for (var key in obj) {
+            e[key] = obj[key]
+        }
+        el.dispatchEvent(e);
+    } else {
+        // IE 8
+        var e = document.createEventObject();
+        e.eventType = type;
+        el.fireEvent('on'+e.eventType, e);
+    }
+}
+
+function movePen(e) {
+    e.preventDefault()
+    var x, y
+    if (penActive) {
+        if (e.type === 'touchmove') {
+            x = e.touches[0].pageX
+            y = e.touches[0].pageY
+        } else {
+            x = e.pageX
+            y = e.pageY
+        }
+    }
+
+    penElement.style.left = "" + x + "px";
+    penElement.style.top = "" + y + "px";
+
+    var span = document.elementsFromPoint(x, y).filter(function(el) {
+        return el.tagName === 'SPAN'
+    })[0]
+
+    if (span !== undefined) {
+        triggerEvent(span, 'mouseover', {
+            pageX: x,
+            pageY: y
+        })
+    }
+}
+
+var penActive = false
+var penElement = document.querySelector('.pen')
+document.addEventListener('DOMContentLoaded', function() {
+    drawScreen()
+    document.querySelector('input[name="reset"]').addEventListener('mousedown', drawScreen)
+    document.querySelector('select').addEventListener('change', changeMode)
+    window.addEventListener("optimizedResize", drawScreen)
+
+    penElement.addEventListener('touchstart', function() { penActive = true })
+    penElement.addEventListener('touchend', function() { penActive = false })
+    penElement.addEventListener('touchmove', movePen)
+    penElement.addEventListener('mousedown', function() { penActive = true })
+    penElement.addEventListener('mouseup', function() { penActive = false })
+    penElement.addEventListener('mousemove', movePen)
+})
