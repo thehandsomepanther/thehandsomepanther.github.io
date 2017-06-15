1a937462d524d5325e6b5f582d3f1460a1963dad
Josh Shi <thehandsomepanther@gmail.com>
2017-06-15 19:19:02 -0400

    elementsfrompoint polyfill

--- scripts/index.js
+++ scripts/index.js
@@ -27,6 +27,25 @@ function removeEventListeners(e) {
     e.parentNode.replaceChild(clone, e)
 }
 
+function allElementsFromPoint(x, y) {
+    var element, elements = [];
+    var old_visibility = [];
+    while (true) {
+        element = document.elementFromPoint(x, y);
+        if (!element || element === document.documentElement) {
+            break;
+        }
+        elements.push(element);
+        old_visibility.push(element.style.visibility);
+        element.style.visibility = 'hidden'; // Temporarily hide the element (without changing the layout)
+    }
+    for (var k = 0; k < elements.length; k++) {
+        elements[k].style.visibility = old_visibility[k];
+    }
+    elements.reverse();
+    return elements;
+}
+
 var THRESHOLD = 5;
 var prev = {
     x: undefined,
@@ -265,7 +284,7 @@ function movePen(e) {
     penElement.style.left = "" + x + "px";
     penElement.style.top = "" + y + "px";
 
-    var span = document.elementsFromPoint(x, y).filter(function(el) {
+    var span = allElementsFromPoint(x, y).filter(function(el) {
         return el.tagName === 'SPAN'
     })[0]
 
