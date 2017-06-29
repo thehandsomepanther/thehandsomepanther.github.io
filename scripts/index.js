6a55a5a0eea23a7f51cf5f8e7c00167f414ea468
Josh Shi <thehandsomepanther@gmail.com>
2017-06-29 15:40:28 -0700

    closest polyfill

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,17 @@
+if (window.Element && !Element.prototype.closest) {
+    Element.prototype.closest = 
+    function(s) {
+        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+            i,
+            el = this;
+        do {
+            i = matches.length;
+            while (--i >= 0 && matches.item(i) !== el) {};
+        } while ((i < 0) && (el = el.parentElement)); 
+        return el;
+    };
+}
+
 function removeEventListeners(e) {
     var clone = e.cloneNode()
     while (e.firstChild) {
